6716725b745c5022e0c7c79a354f284e0ed94336
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:33:01 -0300

    Requirement 01-> Creating user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,10 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
