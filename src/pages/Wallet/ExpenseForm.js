ee3f13f4b3b9b384af896edf350c9b468d1cc708
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 13:13:01 -0300

    Requirement 08-> Controlling the form

--- src/pages/Wallet/ExpenseForm.js
+++ src/pages/Wallet/ExpenseForm.js
@@ -10,34 +10,80 @@ import payMethods from './payMethodsData';
 import expenseTags from './tagsData';
 
 class ExpenseForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '0',
+      description: '',
+      currency: '',
+      method: 'money',
+      tag: 'feeding',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { value, description, currency, method, tag } = this.state;
     const { currencies } = this.props;
     return (
-      <form>
+      <form onSubmit={ this.handleSubmit }>
         <InputNumber
           id="expenseValue"
           label="Valor"
+          name="value"
+          value={ value }
+          onChange={ this.handleChange }
         />
         <InputText
           id="expenseDescribe"
           label="Descrição"
+          name="description"
+          value={ description }
+          onChange={ this.handleChange }
         />
         <Select
           id="expenseCurrency"
           label="Moeda"
           options={ currencies
             .map((currencie) => ({ value: currencie, text: currencie })) }
+          name="currency"
+          valueSelected={ currency }
+          onChange={ this.handleChange }
         />
         <Select
           id="paymentMethod"
           label="Método de pagamento"
           options={ payMethods }
+          name="method"
+          valueSelected={ method }
+          onChange={ this.handleChange }
         />
         <Select
           id="expenseTag"
           label="Tag"
           options={ expenseTags }
+          name="tag"
+          valueSelected={ tag }
+          onChange={ this.handleChange }
         />
+        <button type="submit">
+          Adicionar despesa
+        </button>
       </form>
     );
   }
