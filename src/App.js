23d12753f8afed11e5434886120ce33c647f247c
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:46:53 -0300

    Requirement 04-> Adding route to the `Wallet` page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
+      <Route path="/carteira" component={ Wallet } />
       <Route path="/" component={ Login } />
     </Switch>
   );
