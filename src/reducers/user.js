fc246b3aa821e1a62c4e63be79d3fc8a5d255197
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:40:29 -0300

    Requirement 03-> Adding case `STORE_EMAIL` to the `user` reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,16 @@
+import { STORE_EMAIL } from '../actions/actionsTypes';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case STORE_EMAIL:
+    return {
+      ...state,
+      email: action.email,
+    };
   default:
     return state;
   }
