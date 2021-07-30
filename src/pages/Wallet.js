56ed793dd4611170e5e697faf180e8e543e4b383
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:49:01 -0300

    Requirement 05-> Creating the `header`

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { total } = this.state;
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">
+            { `Email: ${userEmail}` }
+          </span>
+          <span data-testid="total-field">
+            { `Despesa Total: ${total}` }
+          </span>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
