f4fd25e26531f3b75ecd4921e36b0a5febbf7d33
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:57:13 -0300

    Requirement 07-> Creating `wallet` reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { GET_CURRENCIES } from '../actions/actionsTypes';
+
+const INITIAL_STATE = {
+  currencies: [],
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case GET_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.currencies,
+    };
+  default:
+    return state;
+  }
+}
