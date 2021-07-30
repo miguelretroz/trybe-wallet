9932d3ba0041780c6a56a79f50540596835627cd
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-30 11:31:52 -0300

    Requirement 01-> Creanting the login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Password"
+        />
+
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
